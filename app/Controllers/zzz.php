d87e873e2ede787d1051f37d551de6e82ee2e337
itsme <s4kimoll@uni-trier.de>
2022-09-02 11:15:00 +0200

    w/e

---
+++ app/Controllers/zzz.php
@@ -0,0 +1,14 @@
+<?php namespace App\Controllers;
+use App\Models\UserModel;
+use CodeIgniter\Controller;
+
+class zzz extends BaseController {
+    public function __construct() {
+        $this->UserModel = new UserModel();
+    }
+
+    public function index() {
+        # Check if successfully connected to database
+        echo var_dump($this->UserModel->getUsers());
+    }
+}
