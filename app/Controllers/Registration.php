344dbb2d247ca7000af2c7988a3771ca7039b60e
HenriWelsch <s4jowels@uni-trier.de>
2022-09-06 14:20:03 +0200

    Löschen der alten Views

--- app/Controllers/Registration.php
+++ app/Controllers/Registration.php
@@ -3,28 +3,11 @@ use CodeIgniter\Controller;
 
 class Registration extends BaseController {
 
-    public function test1() {
-        $data['title'] = 'Registration';
-
-        echo view('templates/header', $data);
-        echo view('pages/test1');
-        echo view('templates/Footer');
-    }
-
-    public function test2() {
-        $data['title'] = 'Registration';
-
-        echo view('templates/header', $data);
-        echo view('pages/test2');
-        echo view('templates/Footer');
-    }
-
-    public function test3() {
+    public function index() {
         $data['title'] = 'Registration';
 
         echo view('templates/header', $data);
         echo view('pages/test3');
         echo view('templates/Footer');
     }
-
 }
