62d090d23e6e36992e8ed4513c0909c32c4cb41e
itsme <s4kimoll@uni-trier.de>
2022-09-16 23:40:05 +0200

    Implemented the Datepicker as modal

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -12,14 +12,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap -->
+    <script src="https://unpkg.com/jquery@3.5.1/dist/jquery.min.js"></script>
+
     <link href="https://unpkg.com/bootstrap@4.5.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://unpkg.com/bootstrap-table@1.18.0/dist/bootstrap-table.min.css"  rel="stylesheet">
     <script src="https://unpkg.com/bootstrap@4.5.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://unpkg.com/bootstrap-table@1.18.0/dist/bootstrap-table.min.js"></script>
 
-    <script src="https://unpkg.com/jquery@3.5.1/dist/jquery.min.js"></script>
+
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+
+
+    <!-- datepicker -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/css/bootstrap-datepicker.min.css" rel="stylesheet"/>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/js/bootstrap-datepicker.min.js"></script>
 
 </head>
 <!-- End Header ---------------------------------------------------------------------------------------------------------------->
