fbf34dc799e3c10f7bd1f6ae5ef381052666ba56
Xertiler <58569236+Xertiler@users.noreply.github.com>
2022-09-26 18:15:39 +0200

    Revert "Implemented the Datepicker as modal"

--- app/Controllers/WeekOverview.php
+++ app/Controllers/WeekOverview.php
@@ -1,5 +1,4 @@
 <?php namespace App\Controllers;
-use Cassandra\Date;
 use CodeIgniter\Controller;
 
 class WeekOverview extends BaseController {
@@ -16,17 +15,12 @@ class WeekOverview extends BaseController {
         $data['friday'] = date('d.m.Y', strtotime("this week friday"));
         $data['saturday'] = date('d.m.Y', strtotime("this week saturday"));
         $data['sunday'] = date('d.m.Y', strtotime("this week sunday"));
-        $data['my-input'] = date('d.m.Y', strtotime("this week sunday"));
 
         echo view('templates/header', $data);
         echo view('pages/WeekOverview', $data);
         echo view('templates/Footer');
     }
 
-    /**
-     * This method redirects the user to day specific to-do-list.
-     * @return \CodeIgniter\HTTP\RedirectResponse
-     */
     public function button_filter(){
         if (isset($_POST['settings'])) return redirect()->to(base_url() . '/index.php/settings');
         
@@ -59,41 +53,7 @@ class WeekOverview extends BaseController {
         }
     }
 
-    /**
-     * This method calculates and shows the new dates of the week, once a date has been
-     * selected and confirmed in the modal dialog.
-     * @throws \Exception
-     */
-    public function pickDate(){
-        $data['title'] = 'WeekOverview';
-
-        // calculating new calendar week
-        $date = new \DateTime(date('d.m.Y', strtotime($_POST["dateInput"])));
-        $data['week'] = $date->format("W");
-
-        // calculates the date for the individual days of the week
-        $DateTime = new \DateTime();
-        $DateTime -> setISODate(date('Y', strtotime($_POST["dateInput"])), $data['week']);
-
-        $data['monday'] = $DateTime -> format('d.m.Y');
-        $DateTime -> modify('+1 Day');
-        $data['tuesday'] = $DateTime -> format('d.m.Y');
-        $DateTime -> modify('+1 Day');
-        $data['wednesday'] = $DateTime -> format('d.m.Y');
-        $DateTime -> modify('+1 Day');
-        $data['thursday'] = $DateTime -> format('d.m.Y');
-        $DateTime -> modify('+1 Day');
-        $data['friday'] = $DateTime -> format('d.m.Y');
-        $DateTime -> modify('+1 Day');
-        $data['saturday'] = $DateTime -> format('d.m.Y');
-        $DateTime -> modify('+1 Day');
-        $data['sunday'] = $DateTime -> format('d.m.Y');
-        $DateTime -> modify('+1 Day');
-        $data['my-input'] = $DateTime -> format('d.m.Y');
-
-        echo view('templates/header', $data);
-        echo view('pages/WeekOverview', $data);
-        echo view('templates/Footer');
+    public function redirectCalendar(){
+        return redirect()->to(base_url() . '/index.php/registration');  // TODO set correct link once page is created
     }
 }
-
